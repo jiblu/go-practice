b444065f85a02833d43c3b1afa4abab9ecb7f3b2
ji <jkim0107@gmail.com>
2020-02-22 12:18:34 -0800

    added hello test

---
+++ hello/hello_test.go
@@ -0,0 +1,12 @@
+// creating a test for hello.go
+
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want :="Hello, world."
+	if got :=Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
