64606675fa22c5d409b1e6b567e6c3e06918d594
ji <jkim0107@gmail.com>
2020-03-23 19:44:27 -0700

    webscraping practice

---
+++ webScraping/webScraping.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	// "io"
+	"log"
+	"net/http"
+	// "os"
+)
+
+func main() {
+	response, err := http.Get("https://www.devdungeon.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
